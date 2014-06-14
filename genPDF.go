04085334d1d91ed470cd1f513acf0fb89c28a806
Mikolaj Habryn <dichro@rcpt.to>
2014-06-14 15:33:47 -0700

    sprinkling undersized QR codes everywhere

--- genPDF.go
+++ genPDF.go
@@ -1,23 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"code.google.com/p/rsc/qr"
 	"bitbucket.org/zombiezen/gopdf/pdf"
 )
 
 func main() {
-	qrCode, err := qr.Encode("http://foo/bar", qr.H)
-	if err != nil {
-		panic(err)
-	}
-
 	doc := pdf.New()
 	canvas := doc.NewPage(pdf.USLetterWidth, pdf.USLetterHeight)
-	canvas.DrawImage(qrCode.Image(), pdf.Rectangle{
-		Min: pdf.Point{X: 7.5*pdf.Inch, Y: 7.5*pdf.Inch},
-		Max: pdf.Point{X: 8.5*pdf.Inch, Y: 8.5*pdf.Inch},
-	})
+	for row := 0*pdf.Inch; row <= pdf.USLetterHeight-1; row += pdf.Inch {
+		for col := 0*pdf.Inch; col <= pdf.USLetterWidth-1; col += pdf.Inch {
+			qrCode, err := qr.Encode(fmt.Sprintf("http://foo/bar/%d/%d", row, col), qr.H)
+			if err != nil {
+				panic(err)
+			}
+
+			canvas.DrawImage(qrCode.Image(), pdf.Rectangle{
+				Min: pdf.Point{X: col, Y: row},
+				Max: pdf.Point{X: col+pdf.Inch, Y: row+pdf.Inch},
+			})
+		}
+	}
 	canvas.Close()
 
 	if err := doc.Encode(os.Stdout); err != nil {
