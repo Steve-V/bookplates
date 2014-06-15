55201cc6068e1bf84fb534f52aee5adec206e19b
Mikolaj Habryn <dichro@rcpt.to>
2014-06-14 18:57:08 -0700

    gofmt

--- format/writer.go
+++ format/writer.go
@@ -5,14 +5,14 @@ import (
 )
 
 var Avery22805 = LabelSheet{
-	Width: 1.5*pdf.Inch,
-	Height: 1.5*pdf.Inch,
-	PageWidth: 8.5*pdf.Inch,
-	PageHeight: 11*pdf.Inch,
-	Cols: 4,
-	ColGap: 0.3125*pdf.Inch,
-	Rows: 6,
-	RowGap: 0.2*pdf.Inch,
+	Width:      1.5 * pdf.Inch,
+	Height:     1.5 * pdf.Inch,
+	PageWidth:  8.5 * pdf.Inch,
+	PageHeight: 11 * pdf.Inch,
+	Cols:       4,
+	ColGap:     0.3125 * pdf.Inch,
+	Rows:       6,
+	RowGap:     0.2 * pdf.Inch,
 }
 
 type LabelSheet struct {
@@ -30,8 +30,8 @@ type LabelSheet struct {
 func (l *LabelSheet) Positions() []pdf.Rectangle {
 	labels := []pdf.Rectangle{}
 	two := pdf.Unit(2)
-	xBase := (l.PageWidth / two) - pdf.Unit(l.Cols / 2) * (l.Width + l.ColGap) + l.ColGap / two
-	y := (l.PageHeight / two) + pdf.Unit(l.Rows / 2 - 1) * (l.Height + l.RowGap) + l.RowGap / two
+	xBase := (l.PageWidth / two) - pdf.Unit(l.Cols/2)*(l.Width+l.ColGap) + l.ColGap/two
+	y := (l.PageHeight / two) + pdf.Unit(l.Rows/2-1)*(l.Height+l.RowGap) + l.RowGap/two
 	for row := 0; row < l.Rows; row++ {
 		x := xBase
 		for col := 0; col < l.Cols; col++ {
@@ -57,20 +57,20 @@ type Renderer interface {
 }
 
 type LabelSheetWriter struct {
-	paper LabelSheet
+	paper     LabelSheet
 	positions []pdf.Rectangle
-	doc *pdf.Document
-	page *pdf.Canvas
-	written int
+	doc       *pdf.Document
+	page      *pdf.Canvas
+	written   int
 }
 
 func NewLabelSheetWriter(paper LabelSheet) *LabelSheetWriter {
 	pos := paper.Positions()
 	w := &LabelSheetWriter{
-		paper: paper,
+		paper:     paper,
 		positions: pos,
-		doc: pdf.New(),
-		written: len(pos),
+		doc:       pdf.New(),
+		written:   len(pos),
 	}
 	return w
 }
@@ -94,9 +94,9 @@ func (w *LabelSheetWriter) write(renderer Renderer, index int) {
 	w.page.Translate(pos.Min.X, pos.Min.Y)
 	w.written++
 	renderer.Render(w.page, pdf.Point{
-			X: w.paper.Width,
-			Y: w.paper.Height,
-		}, index)
+		X: w.paper.Width,
+		Y: w.paper.Height,
+	}, index)
 	w.page.Pop()
 }
 
@@ -107,4 +107,3 @@ func (w *LabelSheetWriter) Finish() *pdf.Document {
 	}
 	return w.doc
 }
-
--- genPDF.go
+++ genPDF.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"bitbucket.org/zombiezen/gopdf/pdf"
+	"code.google.com/p/rsc/qr"
 	"fmt"
+	"github.com/dichro/bookplates/format"
 	"math"
 	"os"
-	"code.google.com/p/rsc/qr"
-	"bitbucket.org/zombiezen/gopdf/pdf"
-	"github.com/dichro/bookplates/format"
 )
 
 func main() {
@@ -23,14 +23,14 @@ type simple struct {
 }
 
 func (s *simple) Render(page *pdf.Canvas, bound pdf.Point, index int) {
-	qrCode, err := qr.Encode(fmt.Sprintf("http://bcing.me/%d-%d", s.group, s.id + index), qr.L)
+	qrCode, err := qr.Encode(fmt.Sprintf("http://bcing.me/%d-%d", s.group, s.id+index), qr.L)
 	if err != nil {
 		panic(err)
 	}
 	textSize := pdf.Unit(8)
-	qrScale := float32(bound.X - (1.25 * textSize)) / float32(bound.X)
+	qrScale := float32(bound.X-(1.25*textSize)) / float32(bound.X)
 	page.Push()
-	page.Translate(1.25 * textSize, 0)
+	page.Translate(1.25*textSize, 0)
 	page.Scale(qrScale, qrScale)
 	page.DrawImage(qrCode.Image(), pdf.Rectangle{
 		Max: bound,
@@ -45,10 +45,10 @@ func (s *simple) Render(page *pdf.Canvas, bound pdf.Point, index int) {
 	page.Pop()
 	page.Push()
 	page.Translate(textSize, 0)
-	page.Rotate(math.Pi/2)
+	page.Rotate(math.Pi / 2)
 	text = new(pdf.Text)
 	text.SetFont(pdf.Helvetica, textSize)
-	text.Text(fmt.Sprintf("BCID %d-%d", s.group, s.id + index))
+	text.Text(fmt.Sprintf("BCID %d-%d", s.group, s.id+index))
 	page.DrawText(text)
 	page.Pop()
 }
