257ffaff9f797c2b5aa40e3ddb6db796b39ad415
Mikolaj Habryn <dichro@rcpt.to>
2014-06-28 20:21:54 -0700

    lookups

--- upload.pl
+++ upload.pl
@@ -3,12 +3,41 @@
 use strict;
 
 use Text::CSV;
+use WWW::Scraper::ISBN;
 
 my $csv = new Text::CSV;
 my @lines = reverse <>;
+my $scraper = new WWW::Scraper::ISBN;
+$scraper->drivers("LOC", "ISBNnu", "Yahoo");
+
+my $bcid = undef;
+my $book = undef;
+
+# Known elephant in Cairo
+push @lines, "qr,";
 
 foreach my $line (@lines) {
 	$line =~ s/;$//;
-	print $line;
-	print join(";", $csv->fields()), "\n" if $csv->parse($line);
+	unless ($csv->parse($line)) {
+		print "Unparseable", $line, "\n";
+		next;
+	}
+	my ($type, $code) = $csv->fields();
+	if ($type eq "qr") {
+		if ($book) {
+			# upload!
+			print "Uploading", $book->{'title'}, "\n";
+		}
+		$book = undef;
+	} else {
+		if (!$book) {
+			my $r = $scraper->search($code);
+			if ($r->found) {
+				$book = $r->book;
+				print "Found", $code, "\n";
+			} else {
+				print "Not found", $code, "\n";
+			}
+		}
+	}
 }
